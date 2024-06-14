ae30e954b88053cf7ce8cc188a63ebcffc211ffc
StewartMilne <srm@metabureau.com.au>
2024-06-14 15:27:09 +1000

    Add consulting page

--- src/site.config.ts
+++ src/site.config.ts
@@ -43,6 +43,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/posts/",
 		title: "Blog",
 	},
+	{
+		path: "/consulting/",
+		title: "Consulting",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
